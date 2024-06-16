910da29790ba1f6242c732f2bf4bc01ab5081ec0
waseemafzal9353 <waseem.afzal9353@gmail.com>
2024-06-16 13:20:42 +0500

    starting

--- auth/src/index.ts
+++ auth/src/index.ts
@@ -2,8 +2,8 @@ import mongoose from 'mongoose';
 import { app } from './app';
 
 const start = async () => {
-    console.log('Starting app...')
-    
+    console.log('Starting application...')
+
     if (!process.env.JWT_KEY) {
         throw new Error("JWT_KEY must be defined")
     };
